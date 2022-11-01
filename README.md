9c5f0c100fe996e7ea9f0fd5c6d40e96b9d0984e
AlexandrosKap <alexandroskapretsos@gmail.com>
2022-11-01 16:52:04 +0200

    found type bug

--- README.md
+++ README.md
@@ -2,6 +2,26 @@
 
 A simple cheatsheet generator for Nim.
 
+## 🐕 Example
+
+Creates a CHEATSHEET.md file in the current project directory.
+
+```sh
+skonaki .
+```
+
+## 📦 Installation
+
+Copy and paste the following commands into your terminal.
+
+```sh
+nimble install https://github.com/AlexandrosKap/skonaki
+```
+
+## 📖 Documentation
+
+Information on how to use the tool can be found in the CHEATSHEET.md file.
+
 ## 📜 License
 
 The project is released under the terms of the MIT License.
--- src/skonaki.nim
+++ src/skonaki.nim
@@ -1,8 +1,5 @@
 import os, re, strutils, strformat
 
-func findLast(str: string, c: char): int =
-  str.rfind(c)
-
 func ext(path: string): string =
   let extPos = path.searchExtPos
   if extPos > 0:
@@ -21,13 +18,14 @@ func isProc(str: string): bool =
   str.startsWith("macro")
 
 func isType(str: string): bool =
+  (str.startsWith("type") and str[5].isUpperAscii) or
   str.startsWith(re"[A-Z]")
 
 proc writeBlock(doc: File, title: string, lines: seq[string]) =
   if lines.len != 0:
     doc.writeLine(&"{title}\n\n```nim")
     for line in lines:
-      let pos = line.findLast('=')
+      let pos = line.rfind('=')
       doc.writeLine(if pos != -1: line[0 ..< pos] else: line)
     doc.writeLine("```")
 
@@ -61,7 +59,7 @@ proc skonaki*(projectDir = ".", outputDir = ".", name = "CHEATSHEET"): int =
         if pick.isProc:
           procs.add(pick)
         elif pick.isType:
-          types.add(pick)
+          types.add(pick.replace("type", "").strip)
     doc.writeBlock("Types", types)
     if types.len != 0: doc.writeLine("")
     doc.writeBlock("Procedures", procs)
@@ -74,7 +72,7 @@ when isMainModule:
   of 2: skonaki(args[0], args[1])
   of 3: skonaki(args[0], args[1], args[2])
   else: 7
-  
+
   case code:
   of 2: echo "Directory is not a Nim project."
   of 7: echo "Argument list is too long."
